3eea950a62e8595d3b91cfa3c78f849ec47e2422
Mr.YanG <yangyonghai9125@hotmail.com>
2019-11-11 11:43:00 +0800

    feat: tapable in webpack

--- tapable-test/index.js
+++ tapable-test/index.js
@@ -1,11 +1,11 @@
 const { SyncHook } = require('tapable');
 
-const hook = new SyncHook(['arg1', 'arg2', 'arg3']);
+const hook = new SyncHook(['arg1', 'arg2', 'arg3', 'arg4']);
 
 // 绑定事件到webpack事件流
-hook.tap('hook1', (arg1, arg2, arg3) => {
-    console.log(arg1, arg2, arg3);
+hook.tap('hook1', (arg1, arg2, arg3, arg4) => {
+    console.log(arg1, arg2, arg3, arg4);
 });
 
 // 执行绑定事件
-hook.call(1, 2, 3);
+hook.call(1, 2, 3, 4);
